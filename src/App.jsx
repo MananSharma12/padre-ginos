0d3d96473afc59d5863e2f05523f6ed4dad24fff
MananSharma12 <manan.sharma1205@gmail.com>
2025-04-19 19:05:07 +0530

    feat: add backend api

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import './App.css'
+import Pizza from "./components/Pizza.jsx";
 
 function App() {
   return (
-    <>
-    </>
+    <div>
+      <h1>Padre Gino's - Order Now</h1>
+      <Pizza name="Pepperoni" description="Pepperonim Cheese"/>
+      <Pizza name="Hawaiian" description="Ham and Pineapple"/>
+      <Pizza name="Americano" description="French fries, Hot dogs"/>
+    </div>
   )
 }
 
