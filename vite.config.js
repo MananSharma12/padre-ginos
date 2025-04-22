aba22a6d9609b9b57efed9fbe8b512801760c610
MananSharma12 <manan.sharma1205@gmail.com>
2025-04-22 13:48:21 +0530

    feat: add react compiler

--- vite.config.js
+++ vite.config.js
@@ -16,5 +16,16 @@ export default defineConfig({
       }
     }
   },
-  plugins: [TanStackRouterVite(), react()],
+  plugins: [TanStackRouterVite(), react({
+    babel: {
+      plugins: [
+        [
+          "babel-plugin-react-compiler",
+          {
+            target: "19",
+          }
+        ]
+      ]
+    }
+  })],
 })
